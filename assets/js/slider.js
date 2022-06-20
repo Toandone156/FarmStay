c0bd03857304f92a96a498a75ea92d1ba189eec7
Toandone <toanptde160263@fpt.edu.vn>
2022-06-20 12:26:10 +0700

    update pj

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -40,6 +40,7 @@ function showSlides() {
 // }
 
 var mybutton = document.getElementById("myBtn");
+var navbar = document.getElementById("navbar");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -47,8 +48,10 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
+    navbar.style.opacity = 1;
   } else {
     mybutton.style.display = "none";
+    navbar.style.opacity = 0;
   }
 }
 
@@ -71,9 +74,43 @@ function reveal() {
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
     } else {
-      reveals[i].classList.remove("active");
+      // reveals[i].classList.remove("active");
     }
   }
 }
 
 window.addEventListener("scroll", reveal);
+
+// Body
+var body = document.querySelector("body");
+
+// Modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img_links = document.querySelectorAll(".room__content");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+for(var i = 0; i < img_links.length; i++){
+  img_links[i].onclick = function(){
+    var img = this.getElementsByTagName("img")[0];
+    var title = this.querySelector(".room__desc p");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = title.innerHTML;
+    navbar.style.display = "none";
+    // Prevent scroll
+    body.style.overflow="hidden";
+  }
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  navbar.style.display = "block";
+  // Enable scroll
+  body.style.overflow="auto";
+}
